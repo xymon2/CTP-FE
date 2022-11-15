8dbeeeea515569ceb1c90da7e43c1e11cc621ad7
xymon2 <vbbn2030@gmail.com>
2022-11-15 16:02:55 +0900

    FEAT: add signin container and component

--- src/container/SignInFormContainer.tsx
+++ src/container/SignInFormContainer.tsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { postUserInfoForLogin } from "../api/user";
+import SignInForm from "../component/SignInForm";
+import {
+  LoginState,
+  TokenState,
+  UserInfoType,
+  UserState,
+} from "../recoil-state/userState";
+
 const SignInFormContainer = () => {
-  return <></>;
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  const [loggedIn, setLoggedIn] = useRecoilState<boolean>(LoginState);
+  const setUserInfo = useSetRecoilState<UserInfoType>(UserState);
+  const setToken = useSetRecoilState<string>(TokenState);
+
+  const signIn = async () => {
+    try {
+      const token = (await postUserInfoForLogin(id, pw)).data;
+      console.log(token);
+      setToken(token);
+      setLoggedIn(true);
+    } catch (e: any) {
+      alert("Invalid id or password");
+    }
+  };
+
+  return (
+    <div>
+      <SignInForm setId={setId} setPw={setPw} signIn={signIn} />
+    </div>
+  );
 };
 
 export default SignInFormContainer;
